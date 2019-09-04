8eec6411ac557f77f57dca569d8d520305f203ab
Troy McKinnon <troy.t.mckinnon@gmail.com>
2019-09-04 08:31:16 -0500

    Add vscode extensions
    - Replace deprecated tslint with replacement
    - Add spell checker
    - Add markdown linter

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,4 +1,6 @@
 # Install vscode extensions
 code --install-extension ms-vscode.atom-keybindings
-code --install-extension eg2.tslint
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension yzane.markdown-pdf
+code --install-extension ban.spellright
+code --install-extension davidanson.vscode-markdownlint
