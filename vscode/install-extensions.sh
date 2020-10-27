098bb7bfd46b59ba86f57d5d2635d0bb6b39c10b
Troy McKinnon <troy.t.mckinnon@gmail.com>
2020-10-26 21:32:16 -0500

    feat: vscode ext gitlens

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -4,3 +4,4 @@ code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension yzane.markdown-pdf
 code --install-extension ban.spellright
 code --install-extension davidanson.vscode-markdownlint
+code --install-extension eamodio.gitlens
